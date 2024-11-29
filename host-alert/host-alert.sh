ffbc396086eaa99173ec08422382ae8ffba92a93
alexperezortuno <alexperezortuno@gmail.com>
2024-11-29 11:05:36 -0300

    add time stamp

--- host-alert/host-alert.sh
+++ host-alert/host-alert.sh
@@ -9,8 +9,9 @@ echo "Starting host alert script"
 log_file="$(pwd)/${LOG_FILE:-host_alert.log}"
 
 send_telegram_message() {
+  local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
   local url="https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
-  local data="{\"chat_id\": \"$CHAT_ID\", \"text\": \"$1\", \"disable_notification\": true}"
+  local data="{\"chat_id\": \"$CHAT_ID\", \"text\": \"[$timestamp] $1\", \"disable_notification\": true}"
   curl -X POST -H 'Content-Type: application/json' \
   -d  "$data" \
   "$url"
@@ -23,10 +24,10 @@ save_log() {
 ping_to_host() {
   h=$(remove_protocol "$1")
   if ! ping -c 1 "$h" &> /dev/null; then
-    send_telegram_message "[Alert] Host: $h is down"
-    save_log "Host $h is down"
+    send_telegram_message "[Alert] ping to host: $h is timeout"
+    save_log "ping to host $h timeout"
   else
-      save_log "Host $1 is up"
+      save_log "ping to host $1 timeout"
   fi
 }
 
@@ -34,7 +35,7 @@ detect_redirection() {
   # shellcheck disable=SC2155
   local response_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
   echo "Response code: $response_code"
-  if [ "$response_code" -ne 200 ]; then
+  if [ "$response_code" -ne 200 && "$response_code" -ne 000 ]; then
     send_telegram_message "[Alert] Non-200 response: detected for $1 with code $response_code"
     ping_to_host "$1"
   fi
