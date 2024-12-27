182f3cdb7876d21b168d9ea809cd06b93133f589
aperezortuno <alexander.perezortuno@gmail.com>
2024-12-27 09:18:36 -0300

    add result

---
+++ files/README.md
@@ -0,0 +1,14 @@
+### This command find files in a directory with a size greater than a given value.
+```shell
+./find.sh -d /media/alexperezortuno/Workspace/ -m 10
+```
+
+### This command find files in a directory with a size greater than a given value and a given extension.
+```shell
+./find.sh -d /media/alexperezortuno/Workspace/ -m 10 --save
+```
+
+### This command find files in a directory with a size greater than a given value and a given extension and save the result in a file.
+```shell
+ ./find.sh -d /media/alexperezortuno/Workspace/ -m 10 -e jar --save
+```
--- files/find.sh
+++ files/find.sh
@@ -1,17 +1,34 @@
 SIZE_MORE_THAN=0
 SIZE_LESS_THAN=0
+SAVE=0
 SIZE="1"
 MEASURE="M"
 ABS_PATH="/"
+EXTENSION="*"
 
 function get_params() {
   while [[ $# -gt 0 ]]; do
       case "$1" in
           -l|--lt) SIZE_LESS_THAN=1 ;;
           -m|--mt) SIZE_MORE_THAN=1 ;;
-          -s|--size) SIZE="$2" ;;
+          --save) SAVE=1 ;;
+          -s|--size) SIZE="$2"; shift ;;
           -r|--measure) MEASURE="$2"; shift ;;
-          -f|--absolute_path) ABS_PATH="$2"; shift ;;
+          -d|--directory) ABS_PATH="$2"; shift ;;
+          -e|--extension) EXTENSION="$2"; shift ;;
+          -h|--help)
+            echo "Usage: $0 [options]"
+            echo "Options:"
+            echo "  -l, --lt: size less than"
+            echo "  -m, --mt: size more than"
+            echo "  --save: save results to file"
+            echo "  -s, --size: size in MB, use M for MB, G for GB, k for KB"
+            echo "  -r, --measure: measure (K, M, G)"
+            echo "  -d, --directory: directory to search"
+            echo "  -e, --extension: file extension"
+            echo "  -h, --help: display help"
+            exit 0
+            ;;
           \?) echo "Invalid option: -$2" >&2; exit 1 ;;
       esac
       shift
@@ -23,7 +40,15 @@ get_params "$@"
 # find /mnt/d -type f -size +10k -exec ls -lh {} \; | awk '{ print $9 ": " $5 }'
 
 if [ $SIZE_MORE_THAN -eq 1 ]; then
-  find "$ABS_PATH" -type f -size +"$SIZE""$MEASURE"
+  if [ "$SAVE" -eq 1 ]; then
+    find "$ABS_PATH" -type f -size +"$SIZE""$MEASURE" -name "*.$EXTENSION" > results.txt
+  else
+    find "$ABS_PATH" -type f -size +"$SIZE""$MEASURE" -name "*.$EXTENSION"
+  fi
 elif [ $SIZE_LESS_THAN -eq 1 ]; then
-  find "$ABS_PATH" -type f -size -"$SIZE""$MEASURE"
+  if [ "$SAVE" -eq 1 ]; then
+    find "$ABS_PATH" -type f -size -"$SIZE""$MEASURE" -name "*.$EXTENSION" > results.txt
+  else
+    find "$ABS_PATH" -type f -size -"$SIZE""$MEASURE" -name "*.$EXTENSION"
+  fi
 fi
