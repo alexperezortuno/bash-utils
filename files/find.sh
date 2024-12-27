28eb99cd59dce226fce75f6271d179db4838d2a4
alexperezortuno <alexperezortuno@gmail.com>
2024-12-27 08:27:57 -0300

    add find

---
+++ files/find.sh
@@ -0,0 +1,35 @@
+SIZE_MORE_THAN=0
+SIZE_LESS_THAN=0
+SIZE="1"
+MEASURE="M"
+ABS_PATH="/"
+
+function get_params() {
+  while [[ $# -gt 0 ]]; do
+      case "$1" in
+          -l|--lt) SIZE_LESS_THAN=1
+          ;;
+          -m|--mt) SIZE_MORE_THAN=1
+          ;;
+          -s|--size) SIZE="$2"
+          ;;
+          -r|--measure) MEASURE="$2"
+          ;;
+          -f|--absolute_path) ABS_PATH="$2"
+          ;;
+          \?) echo "Invalid option: -$2" >&2 ;;
+      esac
+      shift
+  done
+}
+
+get_params "$@"
+
+# find /mnt/d -type f -size +10k -exec ls -lh {} \; | awk '{ print $9 ": " $5 }'
+echo $SIZE_MORE_THAN
+echo "$ABS_PATH"
+if [ $SIZE_MORE_THAN -eq 1 ]; then
+  find "$ABS_PATH" -type f -size +"$SIZE""$MEASURE"
+elif [ $SIZE_LESS_THAN -eq 1 ]; then
+  find "$ABS_PATH" -type f -size -"$SIZE""$MEASURE"
+fi
